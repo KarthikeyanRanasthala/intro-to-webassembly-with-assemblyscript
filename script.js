a4b9d2a58b5983f4df3dee77f1ad5adc79bb6eda
Karthikeyan Ranasthala <karthikeyan@Karthikeyans-MacBook-Pro.local>
2022-01-26 22:47:51 +0530

    Added slides with reveal.js

---
+++ script.js
@@ -0,0 +1,9 @@
+import Reveal from "/reveal-js/reveal.esm.js";
+import RevealHighlight from "/reveal-js/highlight/highlight.esm.js";
+
+Reveal.initialize({
+    hash: true,
+    slideNumber: true,
+    controlsLayout: "edges",
+    plugins: [ RevealHighlight ]
+});
